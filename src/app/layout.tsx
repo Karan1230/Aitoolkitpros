dfe45c24e3ade89251288ea3a0a6bc5f968ff2e5
ROCKY ROY <rk7681761@gmail.com>
2025-09-05 06:25:01 +0000

    Add this code in website index head tag code  <head></head> tags <meta n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="monetag" content="803be72e60a8dc84014c97c22a08131a" />
         <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpath d='M12 8V4H8'/%3e%3crect width='16' height='12' x='4' y='8' rx='2'/%3e%3cpath d='M2 14h2'/%3e%3cpath d='M20 14h2'/%3e%3cpath d='M15 13v2'/%3e%3cpath d='M9 13v2'/%3e%3c/svg%3e" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
